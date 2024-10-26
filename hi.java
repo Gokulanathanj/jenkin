463c3085fb0dac01cbfe3c1f70d0ae2babfd1784
Gokulanathanj <140598146+Gokulanathanj@users.noreply.github.com>
2024-10-26 14:47:03 +0530

    second.java

---
+++ hi.java
@@ -0,0 +1,9 @@
+
+public class second {
+	public static void main(String[] args) {
+		int number=20;
+		System.out.println("value is:"+number);
+		String student="Gokul";
+		System.out.println("name:"+student);
+	}
+}
